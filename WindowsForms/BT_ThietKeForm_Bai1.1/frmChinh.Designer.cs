48d254d6ed9619600a7e43ac073c1d4bd79a119e
TaiKhoanGitHub411 <danhchogithub@gmail.com>
2025-08-26 16:38:48 +0700

    Xong bài 1 thư mục 2

---
+++ WindowsForms/BT_ThietKeForm_Bai1.1/frmChinh.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace BT_ThietKeForm_Bai1._1
+{
+    partial class frmChinh
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmChinh));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.nguyễnNgọcThanhHiềnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiBai1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.bài2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bài3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nguyễnNgọcThanhHiềnToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(889, 32);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // nguyễnNgọcThanhHiềnToolStripMenuItem
+            // 
+            this.nguyễnNgọcThanhHiềnToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiBai1,
+            this.bài2ToolStripMenuItem,
+            this.bài3ToolStripMenuItem});
+            this.nguyễnNgọcThanhHiềnToolStripMenuItem.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nguyễnNgọcThanhHiềnToolStripMenuItem.Name = "nguyễnNgọcThanhHiềnToolStripMenuItem";
+            this.nguyễnNgọcThanhHiềnToolStripMenuItem.Size = new System.Drawing.Size(215, 28);
+            this.nguyễnNgọcThanhHiềnToolStripMenuItem.Text = "Nguyễn Ngọc Thanh Hiền";
+            // 
+            // tsmiBai1
+            // 
+            this.tsmiBai1.Image = ((System.Drawing.Image)(resources.GetObject("tsmiBai1.Image")));
+            this.tsmiBai1.Name = "tsmiBai1";
+            this.tsmiBai1.Size = new System.Drawing.Size(270, 34);
+            this.tsmiBai1.Text = "Bài 1";
+            // 
+            // bài2ToolStripMenuItem
+            // 
+            this.bài2ToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("bài2ToolStripMenuItem.Image")));
+            this.bài2ToolStripMenuItem.Name = "bài2ToolStripMenuItem";
+            this.bài2ToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.bài2ToolStripMenuItem.Text = "Bài 2";
+            // 
+            // bài3ToolStripMenuItem
+            // 
+            this.bài3ToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("bài3ToolStripMenuItem.Image")));
+            this.bài3ToolStripMenuItem.Name = "bài3ToolStripMenuItem";
+            this.bài3ToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.bài3ToolStripMenuItem.Text = "Bài 3";
+            // 
+            // frmChinh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmChinh";
+            this.Text = "Chương trình chính";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem nguyễnNgọcThanhHiềnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsmiBai1;
+        private System.Windows.Forms.ToolStripMenuItem bài2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bài3ToolStripMenuItem;
+    }
+}
+
