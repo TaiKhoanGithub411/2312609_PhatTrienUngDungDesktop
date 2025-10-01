417579774218af8fd5e2f5fa0ff78f7fb041c5fe
TaiKhoanGitHub411 <danhchogithub@gmail.com>
2025-10-01 10:15:39 +0700

    Xong chức năng các nút thêm, xóa, sửa phần HD1

--- ChuDe4_KetNoiVaTruyXuatDuLieu/Lab_Basic_Command/CategoryForm.Designer.cs
+++ ChuDe4_KetNoiVaTruyXuatDuLieu/Lab_Basic_Command/CategoryForm.Designer.cs
@@ -55,9 +55,10 @@ namespace Lab_Basic_Command
             this.btnLoad.Location = new System.Drawing.Point(12, 112);
             this.btnLoad.Name = "btnLoad";
             this.btnLoad.Size = new System.Drawing.Size(98, 26);
-            this.btnLoad.TabIndex = 0;
+            this.btnLoad.TabIndex = 3;
             this.btnLoad.Text = "Lấy danh sách";
             this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
             // 
             // lvCategory
             // 
@@ -65,6 +66,9 @@ namespace Lab_Basic_Command
             this.chID,
             this.chName,
             this.chType});
+            this.lvCategory.ContextMenuStrip = this.contextMenuStrip1;
+            this.lvCategory.FullRowSelect = true;
+            this.lvCategory.GridLines = true;
             this.lvCategory.HideSelection = false;
             this.lvCategory.Location = new System.Drawing.Point(12, 144);
             this.lvCategory.Name = "lvCategory";
@@ -72,6 +76,7 @@ namespace Lab_Basic_Command
             this.lvCategory.TabIndex = 1;
             this.lvCategory.UseCompatibleStateImageBehavior = false;
             this.lvCategory.View = System.Windows.Forms.View.Details;
+            this.lvCategory.Click += new System.EventHandler(this.lvCategory_Click);
             // 
             // chID
             // 
@@ -91,34 +96,39 @@ namespace Lab_Basic_Command
             this.btnAdd.Location = new System.Drawing.Point(234, 115);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(75, 23);
-            this.btnAdd.TabIndex = 2;
+            this.btnAdd.TabIndex = 4;
             this.btnAdd.Text = "Thêm";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnUpdate
             // 
+            this.btnUpdate.Enabled = false;
             this.btnUpdate.Location = new System.Drawing.Point(336, 115);
             this.btnUpdate.Name = "btnUpdate";
             this.btnUpdate.Size = new System.Drawing.Size(75, 23);
-            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.TabIndex = 5;
             this.btnUpdate.Text = "Cập nhật";
             this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // btnDelete
             // 
+            this.btnDelete.Enabled = false;
             this.btnDelete.Location = new System.Drawing.Point(430, 115);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(75, 23);
-            this.btnDelete.TabIndex = 2;
+            this.btnDelete.TabIndex = 6;
             this.btnDelete.Text = "Xóa";
             this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 14);
+            this.label1.Size = new System.Drawing.Size(86, 22);
             this.label1.TabIndex = 3;
             this.label1.Text = "Mã nhóm";
             // 
@@ -127,7 +137,7 @@ namespace Lab_Basic_Command
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(12, 47);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(105, 14);
+            this.label2.Size = new System.Drawing.Size(154, 22);
             this.label2.TabIndex = 3;
             this.label2.Text = "Tên nhóm thức ăn";
             // 
@@ -136,7 +146,7 @@ namespace Lab_Basic_Command
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(12, 73);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(30, 14);
+            this.label3.Size = new System.Drawing.Size(44, 22);
             this.label3.TabIndex = 3;
             this.label3.Text = "Loại";
             // 
@@ -145,7 +155,8 @@ namespace Lab_Basic_Command
             this.txtID.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtID.Location = new System.Drawing.Point(143, 18);
             this.txtID.Name = "txtID";
-            this.txtID.Size = new System.Drawing.Size(166, 21);
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(166, 27);
             this.txtID.TabIndex = 4;
             // 
             // txtName
@@ -153,41 +164,42 @@ namespace Lab_Basic_Command
             this.txtName.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtName.Location = new System.Drawing.Point(143, 45);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(166, 21);
-            this.txtName.TabIndex = 4;
+            this.txtName.Size = new System.Drawing.Size(166, 27);
+            this.txtName.TabIndex = 1;
             // 
             // txtType
             // 
             this.txtType.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtType.Location = new System.Drawing.Point(143, 71);
             this.txtType.Name = "txtType";
-            this.txtType.Size = new System.Drawing.Size(166, 21);
-            this.txtType.TabIndex = 4;
+            this.txtType.Size = new System.Drawing.Size(166, 27);
+            this.txtType.TabIndex = 2;
             // 
             // contextMenuStrip1
             // 
             this.contextMenuStrip1.Font = new System.Drawing.Font("Roboto", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tsmDelete,
             this.tsmViewFood});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(205, 70);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(268, 60);
             // 
             // tsmDelete
             // 
             this.tsmDelete.Name = "tsmDelete";
-            this.tsmDelete.Size = new System.Drawing.Size(204, 22);
+            this.tsmDelete.Size = new System.Drawing.Size(267, 28);
             this.tsmDelete.Text = "Xóa nhóm sản phẩm";
             // 
             // tsmViewFood
             // 
             this.tsmViewFood.Name = "tsmViewFood";
-            this.tsmViewFood.Size = new System.Drawing.Size(204, 22);
+            this.tsmViewFood.Size = new System.Drawing.Size(267, 28);
             this.tsmViewFood.Text = "Xem danh sách món ăn";
             // 
             // CategoryForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(517, 379);
             this.Controls.Add(this.txtType);
--- ChuDe4_KetNoiVaTruyXuatDuLieu/Lab_Basic_Command/CategoryForm.cs
+++ ChuDe4_KetNoiVaTruyXuatDuLieu/Lab_Basic_Command/CategoryForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,114 @@ namespace Lab_Basic_Command
         public CategoryForm()
         {
             InitializeComponent();
+        }
+        #region Phần mã xử lý khác
+        private void DisplayCategory(SqlDataReader reader)//Hiển thị ds danh mục món ăn lên giao diện
+        {
+            lvCategory.Items.Clear();
+            while (reader.Read())
+            {
+                ListViewItem item = new ListViewItem(reader["ID"].ToString());
+                lvCategory.Items.Add(item);
+                item.SubItems.Add(reader["Name"].ToString());
+                item.SubItems.Add(reader["Type"].ToString());
+            }
+        }
+        #endregion
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=DELL\\SQLEXPRESS;Database=RestaurantManagement;Integrated Security=true;";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            string query = "SELECT ID, Name, Type FROM Category";
+            sqlCommand.CommandText = query;
+            sqlConnection.Open();
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+            this.DisplayCategory(sqlDataReader);
+            sqlConnection.Close();
+        }       
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=DELL\\SQLEXPRESS;Database=RestaurantManagement;Integrated Security=true;";  
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            int typeInt = txtType.Text == "Thức uống" ? 0 : 1;
+            sqlCommand.CommandText = "Insert into Category(Name, [Type])" + "Values(N'" + txtName.Text + "', " + typeInt + ")";
+            sqlConnection.Open();
+            int numOfRowEffected = sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+            if(numOfRowEffected==1)
+            {
+                MessageBox.Show("Thêm nhóm món ăn thành công");
+                btnLoad.PerformClick();
+                txtID.Text = "";
+                txtName.Text = "";
+                txtType.Text = "";
+            }
+        }
+        private void lvCategory_Click(object sender, EventArgs e)//Nhấn xem chi tiết nhóm món ăn
+        {
+            ListViewItem item = lvCategory.SelectedItems[0];
+            txtID.Text = item.Text;
+            txtName.Text = item.SubItems[1].Text;
+            txtType.Text = item.SubItems[1].Text == "0" ? "Thức uống" : "Đồ ăn";//Loại 0 là đồ uống
+
+            btnUpdate.Enabled = true;
+            btnDelete.Enabled = true;
+        }
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=DELL\\SQLEXPRESS;Database=RestaurantManagement;Integrated Security=true;";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            int typeInt = txtType.Text == "Thức uống" ? 0 : 1;//Chuyển chuỗi thành int để lưu loại
+            sqlCommand.CommandText = "Update Category Set Name = N'" + txtName.Text + "', [Type] = N'" + typeInt + "' "+ "Where ID = " + txtID.Text;
+            sqlConnection.Open();
+            int numOfRowEffected = sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+            if(numOfRowEffected==1)
+            {
+                ListViewItem item = lvCategory.SelectedItems[0];
+                item.SubItems[1].Text = txtName.Text;
+                item.SubItems[2].Text = txtType.Text;
+                txtID.Text = "";
+                txtName.Text = "";
+                txtType.Text = "";
+                btnDelete.Enabled = false;
+                btnUpdate.Enabled = false;
+                MessageBox.Show("Cập nhật nhóm món ăn thành công");
+            }
+            else
+            {
+                MessageBox.Show("Có lỗi xảy ra, vui lòng thử lại sau");
+            }
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=DELL\\SQLEXPRESS;Database=RestaurantManagement;Integrated Security=true;";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandText = "Delete from Category where ID = " + txtID.Text;
+            sqlConnection.Open();
+            int numOfRowEffected = sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+            if(numOfRowEffected==1)
+            {                
+                ListViewItem item = lvCategory.SelectedItems[0];
+                item.SubItems[1].Text = txtName.Text;
+                item.SubItems[2].Text = txtType.Text;
+                txtID.Text = "";
+                txtName.Text = "";
+                txtType.Text = "";
+                btnDelete.Enabled = false;
+                btnUpdate.Enabled = false;
+                btnLoad.PerformClick();
+                MessageBox.Show("Xóa nhóm món ăn thành công");
+            }
+            else
+            {
+                MessageBox.Show("Có lỗi, vui lòng thử lại sau");
+            }
         }
     }
 }
