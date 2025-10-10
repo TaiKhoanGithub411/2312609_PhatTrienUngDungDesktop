1f244ae50b91c3cd37fd6deae197be197b67aa72
TaiKhoanGitHub411 <danhchogithub@gmail.com>
2025-10-10 21:38:48 +0700

    datagridview

---
+++ PhatTrienDesktop_OnTap/GiuaKy_OnTap_De3/DocGia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiuaKy_OnTap_De3
+{
+    public class DocGia
+    {
+        public string MaDocGia { get; set; }
+        public string TenDocGia { get; set; }
+        public string SDT { get; set; }
+        public string DiaChi { get; set; }
+        public DocGia() { }
+        public DocGia(string ma, string ten, string sdt, string diachi)
+        {
+            this.MaDocGia = ma;
+            this.TenDocGia = ten;
+            this.SDT = sdt;
+            this.DiaChi = diachi;
+        }
+    }
+}
