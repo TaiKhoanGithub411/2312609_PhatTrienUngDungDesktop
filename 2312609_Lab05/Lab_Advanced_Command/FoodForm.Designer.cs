0ebf1b0a01da5b871fc959b54e3b65523c2be1a6
TaiKhoanGitHub411 <danhchogithub@gmail.com>
2025-10-25 13:46:11 +0700

    Xong Lab05_HuongDan mục 2

---
+++ 2312609_Lab05/Lab_Advanced_Command/FoodForm.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace Lab_Advanced_Command
+{
+    partial class FoodForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbbCategory = new System.Windows.Forms.ComboBox();
+            this.dgvFoodList = new System.Windows.Forms.DataGridView();
+            this.lblQuanlity = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblCatName = new System.Windows.Forms.Label();
+            this.ctmFoodList = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tsmCalculateQuantity = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmSeperator = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmAddFood = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmUpdateFood = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFoodList)).BeginInit();
+            this.ctmFoodList.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chọn nhóm món ăn:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 403);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Có tất cả:";
+            // 
+            // cbbCategory
+            // 
+            this.cbbCategory.FormattingEnabled = true;
+            this.cbbCategory.Location = new System.Drawing.Point(136, 18);
+            this.cbbCategory.Name = "cbbCategory";
+            this.cbbCategory.Size = new System.Drawing.Size(188, 23);
+            this.cbbCategory.TabIndex = 2;
+            this.cbbCategory.SelectedIndexChanged += new System.EventHandler(this.cbbCategory_SelectedIndexChanged);
+            // 
+            // dgvFoodList
+            // 
+            this.dgvFoodList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFoodList.ContextMenuStrip = this.ctmFoodList;
+            this.dgvFoodList.Location = new System.Drawing.Point(3, 58);
+            this.dgvFoodList.Name = "dgvFoodList";
+            this.dgvFoodList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvFoodList.Size = new System.Drawing.Size(514, 330);
+            this.dgvFoodList.TabIndex = 3;
+            // 
+            // lblQuanlity
+            // 
+            this.lblQuanlity.AutoSize = true;
+            this.lblQuanlity.Location = new System.Drawing.Point(77, 403);
+            this.lblQuanlity.Name = "lblQuanlity";
+            this.lblQuanlity.Size = new System.Drawing.Size(16, 15);
+            this.lblQuanlity.TabIndex = 4;
+            this.lblQuanlity.Text = "...";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(132, 404);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "món ăn thuộc nhóm:";
+            // 
+            // lblCatName
+            // 
+            this.lblCatName.AutoSize = true;
+            this.lblCatName.Location = new System.Drawing.Point(257, 404);
+            this.lblCatName.Name = "lblCatName";
+            this.lblCatName.Size = new System.Drawing.Size(16, 15);
+            this.lblCatName.TabIndex = 6;
+            this.lblCatName.Text = "...";
+            // 
+            // ctmFoodList
+            // 
+            this.ctmFoodList.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmCalculateQuantity,
+            this.tsmSeperator,
+            this.tsmAddFood,
+            this.tsmUpdateFood});
+            this.ctmFoodList.Name = "ctmFoodList";
+            this.ctmFoodList.Size = new System.Drawing.Size(186, 92);
+            // 
+            // tsmCalculateQuantity
+            // 
+            this.tsmCalculateQuantity.Name = "tsmCalculateQuantity";
+            this.tsmCalculateQuantity.Size = new System.Drawing.Size(185, 22);
+            this.tsmCalculateQuantity.Text = "Tính số lượng đã bán";
+            this.tsmCalculateQuantity.Click += new System.EventHandler(this.tsmCalculateQuantity_Click);
+            // 
+            // tsmSeperator
+            // 
+            this.tsmSeperator.Name = "tsmSeperator";
+            this.tsmSeperator.Size = new System.Drawing.Size(185, 22);
+            this.tsmSeperator.Text = "----------------------";
+            // 
+            // tsmAddFood
+            // 
+            this.tsmAddFood.Name = "tsmAddFood";
+            this.tsmAddFood.Size = new System.Drawing.Size(185, 22);
+            this.tsmAddFood.Text = "Thêm món ăn mới";
+            // 
+            // tsmUpdateFood
+            // 
+            this.tsmUpdateFood.Name = "tsmUpdateFood";
+            this.tsmUpdateFood.Size = new System.Drawing.Size(185, 22);
+            this.tsmUpdateFood.Text = "Cập nhật món ăn";
+            // 
+            // FoodForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.ClientSize = new System.Drawing.Size(518, 430);
+            this.Controls.Add(this.lblCatName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblQuanlity);
+            this.Controls.Add(this.dgvFoodList);
+            this.Controls.Add(this.cbbCategory);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "FoodForm";
+            this.Text = "Danh sách món ăn";
+            this.Load += new System.EventHandler(this.FoodForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFoodList)).EndInit();
+            this.ctmFoodList.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbbCategory;
+        private System.Windows.Forms.DataGridView dgvFoodList;
+        private System.Windows.Forms.Label lblQuanlity;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblCatName;
+        private System.Windows.Forms.ContextMenuStrip ctmFoodList;
+        private System.Windows.Forms.ToolStripMenuItem tsmCalculateQuantity;
+        private System.Windows.Forms.ToolStripMenuItem tsmSeperator;
+        private System.Windows.Forms.ToolStripMenuItem tsmAddFood;
+        private System.Windows.Forms.ToolStripMenuItem tsmUpdateFood;
+    }
+}
+
---
+++ 2312609_Lab05/Lab_Advanced_Command/FoodForm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Lab_Advanced_Command
+{
+    public partial class FoodForm : Form
+    {
+        private string connectionString = "Server=DELL\\SQLEXPRESS;Database=RestaurantManagement;Integrated Security=true;";
+        private DataTable foodTable;
+        public FoodForm()
+        {
+            InitializeComponent();
+        }
+        #region Các hàm hỗ trợ
+        private void LoadCategory()
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "Select ID, Name from Category";
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            conn.Open();
+            adapter.Fill(dt);
+            conn.Close();
+            conn.Dispose();
+            cbbCategory.DataSource = dt;
+            cbbCategory.DisplayMember = "Name";
+            cbbCategory.ValueMember = "ID";
+        }
+        #endregion
+        private void FoodForm_Load(object sender, EventArgs e)
+        {
+            LoadCategory();
+        }
+
+        private void cbbCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbbCategory.SelectedIndex == -1)
+                return;
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "Select * from Food Where FoodCategoryID=@categoryID";
+            cmd.Parameters.Add("@categoryID", SqlDbType.Int);
+            if(cbbCategory.SelectedValue is DataRowView)
+            {
+                DataRowView rowView = cbbCategory.SelectedValue as DataRowView;
+                cmd.Parameters["@categoryID"].Value = rowView["ID"];
+            }
+            else
+            {
+                cmd.Parameters["@categoryID"].Value = cbbCategory.SelectedValue;
+            }
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            foodTable = new DataTable();
+            conn.Open();
+            adapter.Fill(foodTable);
+            conn.Close();
+            conn.Dispose();
+            dgvFoodList.DataSource = foodTable;
+            lblQuanlity.Text = foodTable.Rows.Count.ToString();
+            lblCatName.Text = cbbCategory.Text;
+        }
+
+        private void tsmCalculateQuantity_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = @"Select @numSaleFood=sum(Quantity) from BillDetails a where a.FoodID=@foodID";
+            if (dgvFoodList.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dgvFoodList.SelectedRows[0];
+                DataRowView rowView = selectedRow.DataBoundItem as DataRowView;
+                cmd.Parameters.Add("@numSaleFood", SqlDbType.Int);
+                cmd.Parameters["@numSaleFood"].Direction = ParameterDirection.Output;
+                cmd.Parameters.Add("@foodID", SqlDbType.Int);
+                cmd.Parameters["@foodID"].Value = rowView["ID"];
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                string result = cmd.Parameters["@numSaleFood"].Value.ToString();
+                MessageBox.Show("Tổng số lượng món " + rowView["Name"] + " đã bán là: " + result + " "+ rowView["Unit"]);
+                conn.Close();
+            }
+            cmd.Dispose();
+            conn.Dispose();
+        }
+    }
+}
