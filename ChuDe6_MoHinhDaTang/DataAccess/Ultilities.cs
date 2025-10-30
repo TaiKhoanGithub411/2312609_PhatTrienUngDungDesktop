a257794eaf0326fb5404e5a622110969cb873a9b
TaiKhoanGitHub411 <danhchogithub@gmail.com>
2025-10-30 08:49:14 +0700

    Xong HD lab06

---
+++ ChuDe6_MoHinhDaTang/DataAccess/Ultilities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace DataAccess
+{
+    public class Ultilities
+    {
+        private static string StrName = "ConnectingStringName";
+        public static string ConnectionString = ConfigurationManager.ConnectionStrings[StrName].ConnectionString;
+        public static string Food_GetAll = "Food_GetAll";
+        public static string Food_InsertUpdateDelete = "Food_InsertUpdateDelete";
+        public static string Category_GetAll = "Category_GetAll";
+        public static string Category_InsertUpDateDelete = "Category_InsertUpDateDelete";
+    }
+}
